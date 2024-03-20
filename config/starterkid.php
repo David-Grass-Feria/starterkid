625b9c1ec81e553852ba80e035529fc8951dda14
David <kontakt@david-grass-feria.de>
2024-03-20 19:07:49 +0100

    1.0

--- config/starterkid.php
+++ config/starterkid.php
@@ -6,10 +6,6 @@ return [
     'search_on_dashboard'                       => env('SEARCH_ON_DASHBOARD', false),
     'enable_notifications'                      => env('ENABLE_NOTIFICATIONS', false),
     'temp_folder'                               => env('TEMP_FOLDER', 'temp'),
-    'primary_color'                             => env('PRIMARY_COLOR', '#F9322C'),
-    'secondary_color'                           => env('SECONDARY_COLOR', '#ffffff'),
-    'font_color'                                => env('FONT_COLOR', '#2e2e2e'),
-    'font_color_on_dark_background'             => env('FONT_COLOR_ON_DARK_BACKGROUND', '#ffffff'),
     'font_familiy'                              => env('FONT_FAMILIY', 'Universal Sans'),
     'time_format'                               => [
 

53d27b2ffadc70022e991ed19be6ccad2a10d8ba
David <kontakt@david-grass-feria.de>
2024-03-20 11:22:09 +0100

    1.0

--- stubs/tailwind.config.js
+++ stubs/tailwind.config.js
@@ -3,7 +3,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} *////
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',

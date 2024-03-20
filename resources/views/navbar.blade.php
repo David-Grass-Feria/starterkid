23480b016f96e3fbb1fe18999bc1f3ee0a4ac84e
David <kontakt@david-grass-feria.de>
2024-03-20 20:02:38 +0100

    Update navbar.blade.php

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -27,7 +27,30 @@
   </x-starterkid::starterkid.nav-link>
   @endcan
 
+  @if(Route::has('services.index'))
+  @can('viewAny',\GrassFeria\StarterkidService\Models\Service::class)
+  <x-starterkid::starterkid.nav-link href="{{route('services.index')}}" :active="request()->routeIs(['services.index','services.create','services.edit'])">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-5 w-5" viewBox="0 0 16 16">
+      <path d="M5 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4m4-2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5M9 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4A.5.5 0 0 1 9 8m1 2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5"/>
+      <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zM1 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H8.96q.04-.245.04-.5C9 10.567 7.21 9 5 9c-2.086 0-3.8 1.398-3.984 3.181A1 1 0 0 1 1 12z"/>
+    </svg>
+        {{__('Services')}}
+    </x-starterkid::starterkid.nav-link>
+    @endcan
+    @endif
 
+
+    @if(Route::has('blogposts.index'))
+    @can('viewAny',\GrassFeria\StarterkidBlog\Models\BlogPost::class)
+    <x-starterkid::starterkid.nav-link href="{{route('blogposts.index')}}" :active="request()->routeIs(['blogposts.index','blogposts.create','blogposts.edit'])">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-5 w-5" viewBox="0 0 16 16">
+        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+      </svg>
+          {{__('Blog')}}
+      </x-starterkid::starterkid.nav-link>
+      @endcan
+      @endif
 
 
 

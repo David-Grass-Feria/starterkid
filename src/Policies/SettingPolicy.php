3846e22ed1dc8e295f439bf9f7484f0e9be035a1
David <kontakt@david-grass-feria.de>
2024-03-24 18:10:21 +0100

    1.0

--- src/Policies/SettingPolicy.php
+++ src/Policies/SettingPolicy.php
@@ -11,7 +11,7 @@ class SettingPolicy
    
     public function update(User $user): bool
     {
-        return $user->role == config('starterkid.global_admin');
+        return $user->role == config('starterkid.global_admin') OR $user->role == 'admin';
     }
 
    

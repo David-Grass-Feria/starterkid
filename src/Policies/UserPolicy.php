7d889c5162f0601b39964af2ee454748b4c4b28a
David <kontakt@david-grass-feria.de>
2024-03-24 18:15:14 +0100

    Update UserPolicy.php

--- src/Policies/UserPolicy.php
+++ src/Policies/UserPolicy.php
@@ -10,25 +10,25 @@ class UserPolicy
    
     public function viewAny(User $user): bool
     {
-        return $user->role == config('starterkid.global_admin');
+        return $user->role == config('starterkid.global_admin') OR $user->role == 'admin';
     }
 
 
    
     public function create(User $user): bool
     {
-        return $user->role == config('starterkid.global_admin');
+        return $user->role == config('starterkid.global_admin') OR $user->role == 'admin';
     }
 
     
     public function update(User $user): bool
     {
-        return $user->role == config('starterkid.global_admin');
+        return $user->role == config('starterkid.global_admin') OR $user->role == 'admin';
     }
 
     public function delete(User $user): bool
     {
-        return $user->role == config('starterkid.global_admin');
+        return $user->role == config('starterkid.global_admin') OR $user->role == 'admin';
     }
 
    

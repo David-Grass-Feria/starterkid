fc2705122d353bb34b1dda3d4b97de806c19f431
David <kontakt@david-grass-feria.de>
2024-03-29 22:22:37 +0100

    Update font.blade.php

--- resources/views/components/starterkid/font.blade.php
+++ resources/views/components/starterkid/font.blade.php
@@ -1,3 +1,3 @@
  <!-- Fonts -->
- <link rel="preconnect" href="https://fonts.bunny.net">
- <link href="https://fonts.bunny.net/css?family={{config('starterkid.font_familiy')}}:400,500,600,700,900&display=swap" rel="stylesheet" />
+<link rel="preload" href="https://fonts.bunny.net/css?family={{config('starterkid.font_familiy')}}:400,500,600,700,900&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript><link href="https://fonts.bunny.net/css?family={{config('starterkid.font_familiy')}}:400,500,600,700,900&display=swap" rel="stylesheet" /></noscript>

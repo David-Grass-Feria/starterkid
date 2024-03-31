52117bd6b04c63962da35c6ee1acbb8e461117a5
David <kontakt@david-grass-feria.de>
2024-03-31 15:46:49 +0200

    Update Setting.php

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -23,33 +23,27 @@ class Setting extends Model implements HasMedia
     {
         $this->addMediaConversion('thumb')
               ->width(config('starterkid.image_conversions.thumb.width'))
-              ->sharpen(config('starterkid.image_conversions.thumb.sharpen'))
               ->quality(config('starterkid.image_conversions.thumb.quality'))
               ->format('webp');
         $this->addMediaConversion('medium')
               ->width(config('starterkid.image_conversions.medium.width'))
-              ->sharpen(config('starterkid.image_conversions.medium.sharpen'))
               ->quality(config('starterkid.image_conversions.medium.quality'))
               ->format('webp');
        $this->addMediaConversion('large')
               ->width(config('starterkid.image_conversions.large.width'))
-              ->sharpen(config('starterkid.image_conversions.large.sharpen'))
               ->quality(config('starterkid.image_conversions.large.quality'))
               ->format('webp');
               
         $this->addMediaConversion('thumb_fallback')
               ->width(config('starterkid.image_conversions.thumb.width'))
-              ->sharpen(config('starterkid.image_conversions.thumb.sharpen'))
               ->quality(config('starterkid.image_conversions.thumb.quality'));
               
         $this->addMediaConversion('medium_fallback')
               ->width(config('starterkid.image_conversions.medium.width'))
-              ->sharpen(config('starterkid.image_conversions.medium.sharpen'))
               ->quality(config('starterkid.image_conversions.medium.quality'));
           
        $this->addMediaConversion('large_fallback')
               ->width(config('starterkid.image_conversions.large.width'))
-              ->sharpen(config('starterkid.image_conversions.large.sharpen'))
               ->quality(config('starterkid.image_conversions.large.quality'));
         
               

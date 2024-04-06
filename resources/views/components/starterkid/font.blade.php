d756d8b23e1c95bcff3425d0ad92a1c898adb55e
David <kontakt@david-grass-feria.de>
2024-04-06 08:02:54 +0200

    1.0

--- resources/views/components/starterkid/font.blade.php
+++ resources/views/components/starterkid/font.blade.php
@@ -1,3 +1,2 @@
   <!-- Fonts -->
-  <link rel="preload" href="https://fonts.bunny.net/css?family={{config('starterkid.font_familiy')}}:400,500,600,700,900&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
-  <noscript><link rel="preload" href="https://fonts.bunny.net/css?family={{config('starterkid.font_familiy')}}:400,500,600,700,900&display=swap" rel="stylesheet" /></noscript>
+
--- stubs/tailwind.config.js
+++ stubs/tailwind.config.js
@@ -3,28 +3,27 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 
-/** @type {import('tailwindcss').Config} *////
+/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './packages/*/*/*/*/*.blade.php',
-        './plugins/*/*/**/*.blade.php',
-        './vendor/grass-feria/starterkid/**/*.blade.php',
-        './vendor/grass-feria/starterkid-frontend/**/*.blade.php',
-        './vendor/grass-feria/starterkid-blog/**/*.blade.php',
-        './vendor/grass-feria/starterkid-service/**/*.blade.php',
+        './packages/grass-feria/starterkid/**/*.blade.php',
+        './packages/grass-feria/starterkid-frontend/**/*.blade.php',
+        './packages/grass-feria/starterkid-service/**/*.blade.php',
+        './plugins/grass-feria/starterkid-tester/**/*.blade.php',
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
+                sans: (process.env.DEFAULT_FONT_FAMILY || 'Nunito, sans-serif').split(',').map(font => font.trim()),
+              },
+              
             colors: {
-                body: process.env.BODY_COLOR || '#f1f5f9',
+                body: process.env.BODY_COLOR || '#f3f4f6',
                 primary: process.env.PRIMARY_COLOR || '#F9322C',
                 secondary: process.env.SECONDARY_COLOR || '#ffffff',
                 font_primary: process.env.FONT_PRIMARY_COLOR || '#2e2e2e',

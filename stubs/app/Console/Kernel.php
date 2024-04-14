b980a002bc6c263e8fbcd252632d43122d05d17c
David <kontakt@david-grass-feria.de>
2024-04-14 06:36:03 +0200

    1.0

--- stubs/app/Console/Kernel.php
+++ stubs/app/Console/Kernel.php
@@ -13,9 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         
-        $schedule->command('backup:run')->daily();
-        $schedule->command('backup:clean')->weekly();
-        $schedule->command('starterkid:clear-temp-folders')->daily();
+       //
     }
 
     /**
---
+++ stubs/routes/console.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+
+//Schedule::command('backup:run')->dailyAt('03:00');
+//Schedule::command('backup:clean')->weekly();
+//Schedule::command('starterkid:clear-temp-folders')->dailyAt('03:00');
+
